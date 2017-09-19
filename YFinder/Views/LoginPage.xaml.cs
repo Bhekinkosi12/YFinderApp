8cf1d371fe8067becec525c826a4b36705872d29
mitchellblom <mitchellblom@gmail.com>
2017-09-19 14:39:54 -0500

    log in user hard code test

--- YFinder/Views/LoginPage.xaml.cs
+++ YFinder/Views/LoginPage.xaml.cs
@@ -1,21 +1,38 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 using Xamarin.Forms;
-using YFinder.Views;
+using YFinder.Models;
 
 namespace YFinder
 {
 	public partial class LoginPage : ContentPage
 	{
-        async void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            await Navigation.PushModalAsync(new UserPage());
-        }
+        private string Url = StaticVariables.YFinderApiRootUrl + "/api/user";
+        private HttpClient _client = new HttpClient();
 
-        public LoginPage()
+		public LoginPage()
 		{
 			InitializeComponent();
+		}
+
+        async void LogThisUserIn(object sender, System.EventArgs e)
+        {
+            var content = await _client.GetStringAsync(Url);
+            var users = JsonConvert.DeserializeObject<List<User>>(content);
+
+            foreach (var user in users)
+            {
+                if (user.userName == "deeveloper")
+                {
+                    var userToLogIn = user;
+                    await Navigation.PushModalAsync(new UserProfilePage(userToLogIn));
+                }
+            }
         }
 	}
 }
