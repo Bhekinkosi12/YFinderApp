6d69fa5034613919405abdfea14494825ef13b16
mitchellblom <mitchellblom@gmail.com>
2017-09-20 17:59:54 -0500

    loc page clone

--- YFinder/Views/MakeReviewPage.xaml.cs
+++ YFinder/Views/MakeReviewPage.xaml.cs
@@ -8,11 +8,16 @@ using Xamarin.Forms.Maps;
 
 namespace YFinder.Views
 {
-    public partial class MakeReviewPage : ContentPage
-    {
-        public MakeReviewPage()
-        {
-		  
-        }
-    }
+	public partial class MakeReviewPage : ContentPage
+	{
+		public MakeReviewPage()
+		{
+			CLLocationManager locationManager = new CLLocationManager();
+			locationManager.RequestWhenInUseAuthorization();
+
+			var latitude = locationManager.Location.Coordinate.Latitude;
+			var longitude = locationManager.Location.Coordinate.Longitude;
+
+		}
+	}
 }
