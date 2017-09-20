87e8d86d5f94e869191e1523c0b54fe744404401
mitchellblom <mitchellblom@gmail.com>
2017-09-20 18:19:37 -0500

    capital letter on tag

--- YFinder/Views/MakeReviewPage.xaml.cs
+++ YFinder/Views/MakeReviewPage.xaml.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using CoreLocation;
 using MapKit;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using YFinder.Models;
+using YFinder.Views;
 
-namespace YFinder.Views
+namespace YFinder
 {
 	public partial class MakeReviewPage : ContentPage
 	{
 		public MakeReviewPage()
 		{
+            InitializeComponent();
+
 			CLLocationManager locationManager = new CLLocationManager();
 			locationManager.RequestWhenInUseAuthorization();
 
 			var latitude = locationManager.Location.Coordinate.Latitude;
 			var longitude = locationManager.Location.Coordinate.Longitude;
-
 		}
 	}
 }
