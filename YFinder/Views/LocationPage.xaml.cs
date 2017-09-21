29c6aa579c23dc579aa59cd287b2fec38869be3a
mitchellblom <mitchellblom@gmail.com>
2017-09-21 18:25:49 -0500

    pins per hotspot

--- YFinder/Views/LocationPage.xaml.cs
+++ YFinder/Views/LocationPage.xaml.cs
@@ -1,60 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using CoreLocation;
 using MapKit;
+using Newtonsoft.Json;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using YFinder.Models;
 
-namespace YFinder.Views
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using YFinder.Views;
+
+namespace YFinder
 {
 	public partial class LocationPage : ContentPage
 	{
+		private string UrlH = StaticVariables.YFinderApiRootUrl + "/api/hotspot";
+		private HttpClient _client = new HttpClient();
+
 		public LocationPage()
 		{
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            var content = await _client.GetStringAsync(UrlH);
+            var hotspots = JsonConvert.DeserializeObject<List<Hotspot>>(content);
+
+			// requesting device permissions for location
 			CLLocationManager locationManager = new CLLocationManager();
 			locationManager.RequestWhenInUseAuthorization();
-
-            // getting device coordinates
-            var latitude = locationManager.Location.Coordinate.Latitude;
-            var longitude = locationManager.Location.Coordinate.Longitude;
-
-            // initiate map object using device coordinates
+			
+			// getting device coordinates
+			var latitude = locationManager.Location.Coordinate.Latitude;
+			var longitude = locationManager.Location.Coordinate.Longitude;
+			
+			// initiate map object using device coordinates
 			var map = new Map(
-			MapSpan.FromCenterAndRadius(
-                    new Position(latitude, longitude), Distance.FromMiles(0.3)))
+				MapSpan.FromCenterAndRadius(
+					new Position(latitude, longitude), Distance.FromMiles(0.3)))
 			{
 				IsShowingUser = true,
 				HeightRequest = 100,
 				WidthRequest = 960,
 				VerticalOptions = LayoutOptions.FillAndExpand
 			};
-
-            // slider for zooming
-            var slider = new Slider(1, 18, 1);
-            slider.ValueChanged += (sender, e) => {
-                var zoomLevel = e.NewValue; // between 1 and 18
-                var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
-                map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
-            };
+			
+			// slider for zooming
+			var slider = new Slider(1, 18, 1);
+			slider.ValueChanged += (sender, e) => {
+				var zoomLevel = e.NewValue; // between 1 and 18
+				var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+				map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+			};
 
             // pins for each hotspot
-			var position = new Position(36.1177254, -86.74888650000003);
-			var pin = new Pin
-			{
-				Type = PinType.Place,
-				Position = position,
-				Label = "Red Bicycle",
-				Address = "something about red bike"
-			};
-			map.Pins.Add(pin);
-
-            // adding all of the above to the view stack
+            foreach (Hotspot hotspot in hotspots)
+            {
+                var position = new Position(hotspot.Latitude, hotspot.Longitude);
+                var pin = new Pin
+                {
+                    Type = PinType.Place,
+                    Position = position,
+                    Label = hotspot.Title,
+                    //Address = "something about red bike"
+                };
+                map.Pins.Add(pin);
+            }
+			
+			// adding all of the above to the view stack
 			var stack = new StackLayout { Spacing = 0 };
 			stack.Children.Add(map);
-            stack.Children.Add(slider);
+			stack.Children.Add(slider);
 			Content = stack;
-
 		}
+
 	}
 }
