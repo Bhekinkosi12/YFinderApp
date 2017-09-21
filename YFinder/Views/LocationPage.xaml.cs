fbedb2cfaa70b635f315eb0f3c27a26bc6bb4b7b
mitchellblom <mitchellblom@gmail.com>
2017-09-21 18:01:04 -0500

    example pin to map

--- YFinder/Views/LocationPage.xaml.cs
+++ YFinder/Views/LocationPage.xaml.cs
@@ -15,9 +15,11 @@ namespace YFinder.Views
 			CLLocationManager locationManager = new CLLocationManager();
 			locationManager.RequestWhenInUseAuthorization();
 
+            // getting device coordinates
             var latitude = locationManager.Location.Coordinate.Latitude;
             var longitude = locationManager.Location.Coordinate.Longitude;
 
+            // initiate map object using device coordinates
 			var map = new Map(
 			MapSpan.FromCenterAndRadius(
                     new Position(latitude, longitude), Distance.FromMiles(0.3)))
@@ -28,14 +30,26 @@ namespace YFinder.Views
 				VerticalOptions = LayoutOptions.FillAndExpand
 			};
 
+            // slider for zooming
             var slider = new Slider(1, 18, 1);
             slider.ValueChanged += (sender, e) => {
-                var zoomLevel = 13; // between 1 and 18
-                slider.Value = 13;
+                var zoomLevel = e.NewValue; // between 1 and 18
                 var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
                 map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
             };
 
+            // pins for each hotspot
+			var position = new Position(36.1177254, -86.74888650000003);
+			var pin = new Pin
+			{
+				Type = PinType.Place,
+				Position = position,
+				Label = "Red Bicycle",
+				Address = "something about red bike"
+			};
+			map.Pins.Add(pin);
+
+            // adding all of the above to the view stack
 			var stack = new StackLayout { Spacing = 0 };
 			stack.Children.Add(map);
             stack.Children.Add(slider);
