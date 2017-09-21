801d68c4fff4d6e37f03e8c9cb0d81dbd9030268
mitchellblom <mitchellblom@gmail.com>
2017-09-21 11:00:25 -0500

    review posting hard code for existing hotspot

--- YFinder/Views/MakeReviewPage.xaml.cs
+++ YFinder/Views/MakeReviewPage.xaml.cs
@@ -23,38 +23,63 @@ namespace YFinder
         public MakeReviewPage()
         {
             InitializeComponent();
+            BindingContext = new NewRating();
         }
 
         async void MakeNewReview(object sender, System.EventArgs e)
         {
+            var hotspot = new NewHotspot(); // new hotspot to be added if doesn't yet exist in db
+            var rating = (NewRating)BindingContext; // binding context from page to create review
+
 			CLLocationManager locationManager = new CLLocationManager();
 			locationManager.RequestWhenInUseAuthorization();
 
 			var latitude = locationManager.Location.Coordinate.Latitude;
 			var longitude = locationManager.Location.Coordinate.Longitude;
 
-            int hotspotIdForRating = 0;
+            string HotspotName = hotspotName.Text;
 
 			var content = await _client.GetStringAsync(UrlH);
             var hotspots = JsonConvert.DeserializeObject<List<Hotspot>>(content);
 
-			foreach (var hotspot in hotspots)
+			foreach (var h in hotspots)
 			{
-				if (hotspot.Title == hotspotName.Text)
+				if (h.Title == HotspotName)
 				{
-                    hotspotIdForRating = hotspot.HotspotId;
-                } else {
-                    var hotspotNew = new NewHotspot();
-                    hotspotNew.Title = hotspotName.Text;
-                    hotspotNew.Latitude = latitude;
-                    hotspotNew.Longitude = longitude;
-					// get the new hotspot's ID back from object
-					// then post below the rating with that hotspot id attached to it.
-                }
+                    rating.HotspotId = h.HotspotId;
+                    rating.Public = 1;
+                    rating.Score = 4;
+                    rating.Speed = (float)6.23;
+					var content3 = JsonConvert.SerializeObject(rating);
+					HttpResponseMessage response1 = await _client.PostAsync(UrlR, new StringContent(content3, Encoding.UTF8, "application/json"));
+					response1.EnsureSuccessStatusCode();
+					string responseBody1 = await response1.Content.ReadAsStringAsync();
+                    await Navigation.PushAsync(new UserProfilePage());
+                    break;
+                } 
             }
 
-            // set rating values here before posting
-
+			var hotspotNew = new NewHotspot();
+			hotspotNew.Title = HotspotName;
+			hotspotNew.Latitude = latitude;
+			hotspotNew.Longitude = longitude;
+			var content2 = JsonConvert.SerializeObject(hotspot);
+			HttpResponseMessage response = await _client.PostAsync(UrlH, new StringContent(content2, Encoding.UTF8, "application/json"));
+			response.EnsureSuccessStatusCode();
+			string responseBody = await response.Content.ReadAsStringAsync();
+			var WantThisToBeHotspotId = JsonConvert.DeserializeObject<Hotspot>(responseBody);
+			
         }
+
+  //      async void PostNewRating()
+  //      {
+		//	var content3 = JsonConvert.SerializeObject(rating);
+		//	HttpResponseMessage response = await _client.PostAsync(UrlR, new StringContent(content3, Encoding.UTF8, "application/json"));
+		//	response.EnsureSuccessStatusCode();
+  //          string responseBody = await response.Content.ReadAsStringAsync();
+		//}
+
+
+
     }
 }
