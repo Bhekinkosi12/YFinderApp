5d9966d36e413561629275f128ad31ddb33795d0
mitchellblom <mitchellblom@gmail.com>
2017-09-21 21:43:54 -0500

    ratings on hotspot detail page

--- YFinder/Views/HotspotDetailPage.xaml.cs
+++ YFinder/Views/HotspotDetailPage.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using YFinder.Models;
 using YFinder.Views;
+using System.Collections.ObjectModel;
 
 namespace YFinder
 {
@@ -11,13 +15,28 @@ namespace YFinder
     public partial class HotspotDetailPage : ContentPage
     {
 		private Hotspot _hotspotPassed = new Hotspot();
+        private string UrlR = StaticVariables.YFinderApiRootUrl + "/api/rating";
+        private HttpClient _client = new HttpClient();
+        private ObservableCollection<Rating> _ratings;
 
 		public HotspotDetailPage(Hotspot hotspot)
 		{
 			InitializeComponent();
-			BindingContext = hotspot;
+            BindingContext = hotspot;
             _hotspotPassed = hotspot;
 		}
+
+        protected override async void OnAppearing()
+        {
+			var content = await _client.GetStringAsync(UrlR);
+			var ratings = JsonConvert.DeserializeObject<List<Rating>>(content);
+            var matchingRatings = ratings.Where(r => r.HotspotId == _hotspotPassed.HotspotId);
+
+			_ratings = new ObservableCollection<Rating>(matchingRatings);
+			ratingsListView.ItemsSource = _ratings;
+
+			base.OnAppearing();
+        }
 
         async void ClearDetail(object sender, System.EventArgs e)
         {
