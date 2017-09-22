e18c9930213b081e39f7941961d0bcc02418f38e
mitchellblom <mitchellblom@gmail.com>
2017-09-21 21:13:28 -0500

    passing hotspot coords for directions

--- YFinder/Views/HotspotDetailPage.xaml.cs
+++ YFinder/Views/HotspotDetailPage.xaml.cs
@@ -10,10 +10,13 @@ namespace YFinder
 
     public partial class HotspotDetailPage : ContentPage
     {
+		private Hotspot _hotspotPassed = new Hotspot();
+
 		public HotspotDetailPage(Hotspot hotspot)
 		{
 			InitializeComponent();
 			BindingContext = hotspot;
+            _hotspotPassed = hotspot;
 		}
 
         async void ClearDetail(object sender, System.EventArgs e)
@@ -24,7 +27,7 @@ namespace YFinder
 		async void TakeMeThere(object sender, System.EventArgs e)
 		{
 			Device.OpenUri(
-			new Uri(string.Format ("http://maps.apple.com/?q={0}", WebUtility.UrlEncode("912 Ireland St Nashville TN"))));
+                new Uri(string.Format("http://maps.apple.com/?q={0}", WebUtility.UrlEncode(_hotspotPassed.Latitude + " " + _hotspotPassed.Longitude))));
 		}
 		
     }
